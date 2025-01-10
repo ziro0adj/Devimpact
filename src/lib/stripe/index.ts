15a75f318c56e78e529e54d91ede8519ae4181ed
ilias <ziro0dz@gmail.com>
2025-01-10 15:41:49 +0000

    .

--- src/lib/stripe/index.ts
+++ src/lib/stripe/index.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
-  apiVersion: '2025-01-01',
+  apiVersion: '2024-12-18.acacia',
   appInfo: {
     name: 'Web up',
     version: '0.1.0',
